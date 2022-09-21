5d8346d05e7054a8ca036eba816649583b269916
Alex Gavliuk <ext-oleksii.gavliuk@here.com>
2022-09-21 11:47:21 +0300

    Add initial draft of the project template

---
+++ intellistart-java-2022-dreamteam/src/main/java/ua/intellias/intellistart/interviewplanning/InterviewPlanningApplication.java
@@ -0,0 +1,13 @@
+package ua.intellias.intellistart.interviewplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPlanningApplication.class, args);
+	}
+
+}
