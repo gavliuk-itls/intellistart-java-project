11610954d4dc4951c91173c4bdede36a730dd22b
Alex Gavluk <gavluk@gmail.com>
2022-10-01 16:28:51 +0300

    :card_file_box: Fix package name (ua -> com) (#9)
    Co-authored-by: Alex Gavliuk <ext-oleksii.gavliuk@here.com>

--- intellistart-java-2022-dreamteam/src/main/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplication.java
+++ intellistart-java-2022-dreamteam/src/main/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplication.java
@@ -1,4 +1,4 @@
-package ua.intellias.intellistart.interviewplanning;
+package com.intellias.intellistart.interviewplanning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
