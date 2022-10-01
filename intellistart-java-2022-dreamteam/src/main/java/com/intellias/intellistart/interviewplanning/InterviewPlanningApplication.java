53411cd5426eb63e16dcca91b0a7110a600efa68
Alex Gavluk <gavluk@gmail.com>
2022-10-01 17:09:51 +0300

    :necktie: Add codestyle checks (#10)
    Co-authored-by: Alex Gavliuk <ext-oleksii.gavliuk@here.com>

--- intellistart-java-2022-dreamteam/src/main/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplication.java
+++ intellistart-java-2022-dreamteam/src/main/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplication.java
@@ -3,11 +3,14 @@ package com.intellias.intellistart.interviewplanning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application.
+ */
 @SpringBootApplication
 public class InterviewPlanningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InterviewPlanningApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(InterviewPlanningApplication.class, args);
+  }
 
 }
